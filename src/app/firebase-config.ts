8015626483e113f8ced1089c625a80a80dc2bf34
Aidan Pietruszka <apie13soccer@gmail.com>
2026-09-15 02:18:15 -0400

    Modified Firebase Config

--- src/app/firebase-config.ts
+++ src/app/firebase-config.ts
@@ -7,14 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyBy9WonFeK3d9ZEuDGDQtR3TdRldZ0SU3w",
-  authDomain: "mobileappsprogram-6e65c.firebaseapp.com",
-  databaseURL: "https://mobileappsprogram-6e65c-default-rtdb.firebaseio.com/",
-  projectId: "mobileappsprogram-6e65c",
-  storageBucket: "mobileappsprogram-6e65c.firebasestorage.app",
-  messagingSenderId: "588329493891",
-  appId: "1:588329493891:web:9dc57909591751244a42b6",
-  measurementId: "G-D4FNHTGQFD"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
